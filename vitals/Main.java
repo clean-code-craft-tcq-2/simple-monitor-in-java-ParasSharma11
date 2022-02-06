f54f6095f939a782201e16427c4fcaff07fa81e1
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-02-06 23:58:06 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,14 +1,31 @@
 package vitals;
-
-public class Main {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
+    
+public class Main implements monitorInterface{
+    static boolean batteryIsOk(float temperature, float soc, float chargeRate, monitorInterface mI) {
+        if(mI.tempCheck(temperature) && mI.socCheck(soc) && mI.chargeRateCheck(chargeRate)) {
+        return true;
+        }
+        else return false;
+    }
+    
+    public boolean tempCheck(float temperature){
+         if(temperature < 0 || temperature > 45) {
+             System.out.println("Temperature is out of range!");
+             return false;
+         }
+        return true;
+    }
+    
+    public boolean socCheck(float soc){
+        if(soc < 20 || soc > 80) {
             System.out.println("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        }
+        return true;
+    }
+    
+    public boolean chargeRateCheck(float chargeRate){
+        if(chargeRate > 0.8) {
             System.out.println("Charge Rate is out of range!");
             return false;
         }
@@ -16,8 +33,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
+        monitorInterface mI = new simpleMonitor();
+        assert(batteryIsOk(42,70,0.07f,mI) == true);
+        assert(batteryIsOk(50, 85, 0.0f,mI) == false);
+        //java -ea alerter.java
         System.out.println("Some more tests needed");
     }
 }
+
+
