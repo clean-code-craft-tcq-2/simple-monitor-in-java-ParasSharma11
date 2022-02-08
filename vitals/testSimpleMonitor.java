1ecc9a62a07d8b6e741d17171eb97b76a090d890
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-02-08 20:01:34 +0530

    Update testSimpleMonitor.java

--- vitals/testSimpleMonitor.java
+++ vitals/testSimpleMonitor.java
@@ -1,8 +1,36 @@
 package vitals;
 public class testSimpleMonitor   {
-    public static void main(String[] args) {
-        simpleMonitor sM = new simpleMonitor();
-        assert(sM.batteryIsOk(sM.tempCheck(50), sM.socCheck(85), sM.chargeRateCheck(0.0f)) == false);
-        assert(sM.batteryIsOk(sM.tempCheck(42), sM.socCheck(70), sM.chargeRateCheck(0.07f) == true));
+    simpleMonitor mI = new simpleMonitor();
+    public void  test_temperature_ok(float temperature,boolean expected_result) {
+        assert(mI.tempCheck(temperature) == expected_result);
     }
+    
+    public void  test_SOC_ok(float soc,boolean expected_result) {
+        assert(mI.socCheck(soc) == expected_result);
+    }
+    
+    public void  test_chargeRate_ok(float chargeRate,boolean expected_result) {
+        assert(mI.chargeRateCheck(chargeRate) == expected_result);
+    }
+    
+    public void temperature_validation() {
+      test_temperature_ok(0,true);
+      test_temperature_ok(45,true);
+      test_temperature_ok(-1,false);
+    }
+    public void SOC_validation() {
+      test_SOC_ok(25,true);
+      test_SOC_ok(40,true);
+      test_SOC_ok(10,false);
+      }
+    public void Charge_rate_validation() {
+        test_chargeRate_ok(0.5f,true);
+        test_chargeRate_ok(0.4f,true);
+        test_chargeRate_ok(0.9f,false);
+        }
+    
+    public void   test_battery_ok1() {
+        assert(mI.batteryIsOk(mI.tempCheck(50), mI.socCheck(85), mI.chargeRateCheck(0.0f)) == false);
+        assert(mI.batteryIsOk(mI.tempCheck(42), mI.socCheck(70), mI.chargeRateCheck(0.07f) == true));
+        }
 }
