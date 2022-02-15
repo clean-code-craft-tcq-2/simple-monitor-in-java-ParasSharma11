1d96b3a7eefa09d2638d700d1e646da8a3d92d55
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-02-15 21:53:40 +0530

    Delete BatteryParameters.java

--- vitals/BatteryParameters.java
+++
@@ -1,7 +0,0 @@
-package vitals;
-
-public interface BatteryParameters {
-public boolean tempCheck(float temp);
-public boolean socCheck(float soc);
-public boolean chargeRateCheck(float chargeRate);
-}
